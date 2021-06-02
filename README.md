793a698e932fd150e6d2debff0feb1059aafa55e
wei.ma1 <wei.ma1@nio.com>
2021-06-02 15:01:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# console-log-node
+console log nodejs
+
+### Install
+Install with npm:
+
+`$ npm install --save console-log-cmd`
+
+### Examples
+
+```
+const { info, success, warn, error} = require('./log');
+info('info!');
+success('success!');
+error('error!');
+error('error!');
+```
---
+++ src/index.js
@@ -0,0 +1,22 @@
+function info(message){
+  console.log('\x1B[37m%s\x1B[0m', `☺${message}`);
+}
+
+function success(message){
+  console.log('\x1B[32m%s\x1B[0m', `✔${message}`);
+}
+
+function warn(message){
+  console.log('\x1B[33m%s\x1B[0m', `⚠${message}`);
+}
+
+function error(message){
+  console.log('\x1B[31m%s\x1B[0m', `✘${message}`);
+}
+
+module.exports = {
+  info,
+  success,
+  warn,
+  error
+};
