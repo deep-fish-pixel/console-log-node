cfc91c9bc5459557987632bcfe001c25de0d2835
wei.ma1 <wei.ma1@nio.com>
2021-11-02 18:12:56 +0800

    修改日志级别

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
+function log(...messages){
+  console.log('\x1B[37m%s\x1B[0m', messages.join(' '));
+}
+
 function info(...messages){
-  console.log('\x1B[37m%s\x1B[0m', `☺${messages.join(' ')}`);
+  console.info('\x1B[37m%s\x1B[0m', `☺${messages.join(' ')}`);
 }
 
 function success(...messages){
-  console.log('\x1B[32m%s\x1B[0m', `✔${messages.join(' ')}`);
+  console.info('\x1B[32m%s\x1B[0m', `✔${messages.join(' ')}`);
 }
 
 function warn(...messages){
-  console.log('\x1B[33m%s\x1B[0m', `⚠${messages.join(' ')}`);
+  console.warn('\x1B[33m%s\x1B[0m', `⚠${messages.join(' ')}`);
 }
 
 function error(...messages){
-  console.log('\x1B[31m%s\x1B[0m', `✘${messages.join(' ')}`);
+  console.error('\x1B[31m%s\x1B[0m', `✘${messages.join(' ')}`);
 }
 
 module.exports = {
+  log,
   info,
   success,
   warn,
